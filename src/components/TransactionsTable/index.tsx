57cd4233135c1dd96888433f4ea6e745f73e18a5
Thiago Dev <thiagodevprogramador@gmail.com>
2022-10-01 12:11:07 -0300

    :tada: first commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+
+  useEffect(() =>{
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.100</td>
+            <td>Casa</td>
+            <td>05/02/2021</td>
+          </tr>
+
+          <tr>
+            <td>Venda</td>
+            <td className="deposit">R$8.000</td>
+            <td>Motocicleta</td>
+            <td>05/05/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
